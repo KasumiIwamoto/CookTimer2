0e26e2347d44c76bc9edc47ac74961813485ff19
岩本果純 <iwamotokasumi@iwamotokajun-no-MacBook-Air.local>
2015-03-03 22:56:55 +0900

    Can select movie

---
+++ CookTimer2/Movie2ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  Movie2ViewController.swift
+//  CookTimer2
+//
+//  Created by 岩本果純 on 2015/03/03.
+//  Copyright (c) 2015年 KasumiIwamoto. All rights reserved.
+//
+
+import UIKit
+
+class Movie2ViewController: UIViewController {
+    
+    @IBOutlet var myWebView : UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        myWebView.delegate = self
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        let url : NSURL = NSURL(string : "https://www.youtube.com/embed/IOtr8Tp_SLs")!
+        let request : NSURLRequest = NSURLRequest(URL: url)
+        myWebView.loadRequest(request)
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView!) {
+        println("webViewDidFinishLoad")
+    }
+    
+    func webViewDidStartLoad(webView: UIWebView!) {
+        println("webViewDidStartLoad")
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
---
+++ CookTimer2/Movie6ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Movie6ViewController.swift
+//  CookTimer2
+//
+//  Created by 岩本果純 on 2015/03/03.
+//  Copyright (c) 2015年 KasumiIwamoto. All rights reserved.
+//
+
+import UIKit
+
+class Movie6ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
